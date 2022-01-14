5384258c9bed16e3553bbb755c0e1ce88c3620ae
AlexIlichev <sapfir8500@mail.ru>
2022-01-14 10:41:34 +0400

    modified index.html.erb, delete turbolinks, modified application.html.erb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ActiveRecord::Base
+  PRIORITIES = [
+['Later', 1],
+['Next', 2],
+['Now', 3]
+  ]
+
+def complete!
+  self.comleted = true
+  save
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks
+ root 'task#index'
+ 
+  resources :tasks do
+    put :complete, on: :member
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
